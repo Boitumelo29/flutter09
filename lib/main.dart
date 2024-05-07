81eae56ef89a334f151ef09aa71173c9f005707a
Boitumelo29 <77927688+Boitumelo29@users.noreply.github.com>
2024-05-07 15:32:43 +0200

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,51 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter/material.dart';
-
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatefulWidget {
   @override
-  State<MyApp> createState() => _MyAppState();
+  State<StatefulWidget> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
+  // final _screenshotController = ScreenshotController();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello App'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [],
-          ),
-        ),
-      ),
-    );
+        home: Scaffold(
+            body: Center(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+          // Screenshot(
+          //   controller: _screenshotController,
+          //   child: Card(
+          //       child: Padding(
+          //     padding: EdgeInsets.all(16),
+          //     child: Column(
+          //       children: [
+          //         Image.asset('images/codepassionately.png'),
+          //         Text(
+          //           'Code Passionately',
+          //           style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          //         ),
+          //       ],
+          //     ),
+          //   )),
+          // ),
+          TextButton(
+            child: Text('Take Screenshot and Share'),
+            onPressed: () {},
+            // onPressed: _takeScreenshot,
+          )
+        ]))));
   }
+
+  // void _takeScreenshot() async {
+  //   final imageFile = await _screenshotController.capture();
+  //   Share.shareFiles([imageFile.path], text: "Shared from #SexyFlutterApp");
+  // }
 }
